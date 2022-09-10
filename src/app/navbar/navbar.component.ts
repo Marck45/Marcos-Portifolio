f9b72106636cc1fa52f631bb9bee0529c713fa4a
Marck45 <marcosantcarvalho99@gmail.com>
2022-09-10 00:13:22 -0300

    atualzição de layout mobile

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+    menuBar = document.getElementById("menu-bar") as HTMLElement;
+    nav = document.getElementById("nav") as HTMLElement;
+    menuBg = document.getElementById("menu-bg") as HTMLElement;
 
-  constructor() { }
+
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.menuBar = document.getElementById("menu-bar") as HTMLElement;
+    this.nav = document.getElementById("nav") as HTMLElement;
+    this.menuBg = document.getElementById("menu-bg") as HTMLElement;
 
 
   }
 
+  
+  menuOnClick() {
+      this.menuBar.classList.toggle("change");
+      this.nav.classList.toggle("change");
+      this.menuBg.classList.toggle("change-bg");
+      
+  }
 }
